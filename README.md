e4afd1272e26be813ff3b448262a941fec5bdae7
HuanSwallo <tannernguyen561@gmail.com>
2025-07-07 23:31:34 -0700

    Finished step 6-7 and extra credit. Edited html, css and README files.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# calculator
+# Calculator App
+A project where I used all of my foundation knowledge of web development into creating a calculator that uses html, css, and js.
+
+# How to Use
+Click on a number to have the first operand used for the operation. Then, click on an operator to execute with the operands. Next, click on a number for the second operand to perform the equation. Lastly, click on the equal sign to solve the equation you inputted. You can also perform multiple operations to help solve for longer equations. To clear the whole calculator, click on the AC button. To delete a recent number, click on the DEL button. It also has keyboard support where you can type your equations to the calculator insead of clicking the buttons.
+
+# What I have learned
+1. Create functions that calls upon one another.
+2. Creating error cases for inputting numbers and operators.
+3. Learn how to use keyboard support.
--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function operate (operator, leftNum, rightNum) {
                 answer = divide(leftNum, rightNum);
             } else {
                 alert("You cannot divide by zero!")
+                resetValues();
                 answer = 0;
             }
         }     
@@ -50,6 +51,12 @@ function resetValues() {
     rightOperand = undefined;
     operator = undefined;
     result = undefined;
+    haveMultipleOperators = false;
+}
+
+function getResult() {
+    rightOperand = parseFloat(display.textContent);
+    return parseFloat(operate(operator, leftOperand, rightOperand).toFixed(3));
 }
 
 const display = document.querySelector(".display");
@@ -58,6 +65,10 @@ let leftOperand, rightOperand, operator, result, haveMultipleOperators = false;
 const numberPad = document.querySelectorAll("button.numbers");
 numberPad.forEach(button => {
     button.addEventListener('click', () => {
+        if (haveMultipleOperators) {
+            clearDisplay();
+            haveMultipleOperators = false;
+        }
         display.textContent += button.textContent;
     });
 });
@@ -65,10 +76,22 @@ numberPad.forEach(button => {
 const operatorPad = document.querySelectorAll("button.operators");
 operatorPad.forEach(button => {
     button.addEventListener('click', () => {
+        // Case when leftOperand or operator is not defined (1 +)
         if (leftOperand === undefined && operator === undefined && display.textContent !== "") {
             leftOperand = parseFloat(display.textContent);
             operator = button.textContent;
             clearDisplay();
+        // Case when have multiple operators (1+1+)    
+        } else if (leftOperand !== undefined && operator !== undefined && display.textContent !== "" && !haveMultipleOperators) {
+            result = getResult();
+            clearDisplay();
+            display.textContent += result;
+            haveMultipleOperators = true;
+            leftOperand = result;
+            operator = button.textContent
+         // Case when operators pressed continuosly, uses last operator pressed (1+-)   
+        } else if (leftOperand !== undefined && operator !== undefined && display.textContent === "") {
+            operator = button.textContent;
         }
     })
 });
@@ -76,17 +99,26 @@ operatorPad.forEach(button => {
 const decimalKey = document.querySelectorAll("#decimal");
 decimalKey.forEach(button => {
     button.addEventListener('click', () => {
-        display.textContent += button.textContent;
+        //Prevents having multiple decimal points
+        if (!display.textContent.includes('.')) {
+            // Displays 0 with . if no number before that
+            if (display.textContent === "") {
+                display.textContent += '0.';
+            } else {
+                display.textContent += button.textContent;
+            }
+        }
     })
 });
 
 const equalKey = document.querySelector("#equal");
 equalKey.addEventListener('click', () => {
+    // Case when there is a leftOperand and operator
     if (leftOperand != undefined && operator != undefined && display.textContent != "") {
-        rightOperand = parseFloat(display.textContent);
-        result = parseFloat(operate(operator, leftOperand, rightOperand));
+        result = getResult();
         clearDisplay();
         display.textContent += result;
+        resetValues();
     }
 });
 
@@ -101,3 +133,65 @@ deleteKey.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0,-1); 
 });
 
+//Keyboard Support
+let clickEvent = new Event("click");
+
+window.addEventListener('keydown', function(e) {
+    let currentKey;
+
+    switch (e.key) {
+        case '0':
+            currentKey = this.document.querySelector("#zero");
+            break;
+        case '1':
+            currentKey = this.document.querySelector("#one");
+            break;
+        case '2':
+            currentKey = this.document.querySelector("#two");
+            break;
+        case '3':
+            currentKey = this.document.querySelector("#three");
+            break;
+        case '4':
+            currentKey = this.document.querySelector("#four");
+            break;
+        case '5':
+            currentKey = this.document.querySelector("#five");
+            break;
+        case '6':
+            currentKey = this.document.querySelector("#six");
+            break;
+        case '7':
+            currentKey = this.document.querySelector("#seven");
+            break;
+        case '8':
+            currentKey = this.document.querySelector("#eight");
+            break;
+        case '9':
+            currentKey = this.document.querySelector("#nine");
+            break;
+        case '+':
+            currentKey = this.document.querySelector("#add");
+            break;   
+        case '-':
+            currentKey = this.document.querySelector("#substract");
+            break;
+        case '*':
+            currentKey = this.document.querySelector("#multiply");
+            break;
+        case '/':
+            currentKey = this.document.querySelector("#divide");
+            break;
+        case '.':
+            currentKey = this.document.querySelector("#decimal");
+        case '=':
+        case 'Enter':
+            currentKey = this.document.querySelector("#equal");
+            break;
+        case 'Backspace':
+            currentKey = this.document.querySelector("#delete");  
+    }
+    if (currentKey) {
+        currentKey.dispatchEvent(clickEvent);
+    }
+});
